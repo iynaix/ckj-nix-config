e88d8af7908097c0a5423a4e69e3fef249d58692
CerealKillerJohn <jwrhine@proton.me>
2024-01-09 22:12:27 -0500

    before common config

--- modules/desktops/gnome.nix
+++ modules/desktops/gnome.nix
@@ -32,6 +32,7 @@
       gnomeExtensions.media-controls
       gnomeExtensions.notification-timeout
       gnomeExtensions.only-window-maximize
+      gnomeExtensions.tailscale-status
       gnome-extension-manager
       ];
   };
