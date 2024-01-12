4ebdac64e1ed71b7900238c663f39978690204da
CerealKillerJohn <jwrhine@proton.me>
2024-01-12 18:39:22 -0500

    fixed imports

--- packages/sops.nix
+++ packages/sops.nix
@@ -7,7 +7,7 @@
       inputs.sops-nix.nixosModules.sops
     ];
 
-  sops.defaultSopsFile = ../../secrets/secrets.yaml;
+  sops.defaultSopsFile = ../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
   
   sops.age.keyFile = "/home/jwrhine/.config/sops/age/keys.txt";
