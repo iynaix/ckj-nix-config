618ba4f3149d4da5ec623d2dc929e198b3fd4948
CerealKillerJohn <JWRhine@proton.me>
2024-01-16 23:27:52 -0500

    fixed gdm autologin

--- configuration.nix
+++ configuration.nix
@@ -102,6 +102,13 @@
     };
   };  
 
+  systemd = {
+    services = {
+      "getty@tty1".enable = false; # fixes autologin with gdm
+      "autovt@tty1".enable = false; # https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+    };
+  };
+
   system = {
     stateVersion = "23.11";
   };
