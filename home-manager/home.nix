acae23113876226ba08b624f4b41845a27158e43
CerealKillerJohn <JWRhine@proton.me>
2024-01-18 12:38:41 -0500

    updated common home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,32 +1,6 @@
 { config, pkgs, lib, ... }:
 
 {
-  programs = {
-    nano = {
-      enable = false;
-    };
-  };
-  
-  environment = {
-    variables = {
-     EDITOR = "nvim";
-    };
-    shells = with pkgs; [ zsh ];
-    systemPackages = with pkgs; [
-      cbonsai
-      fastfetch
-      firefox
-      kitty
-      lm_sensors
-      lsd
-      mako
-      neofetch
-      neovim
-      wallust
-      wget
-    ];
-  };
-
   xdg = {
     configFile = {
       "kitty/kitty.conf" = {
