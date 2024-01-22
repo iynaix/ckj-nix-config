6aff53cee8a417f64b14f95dfe520b8f4b9381dd
CerealKillerJohn <JWRhine@proton.me>
2024-01-22 09:13:48 -0500

    working with fonts

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -3,16 +3,16 @@
 {
   fonts = {
     packages = with pkgs; [
-      corefonts
-      dina-font
-      fira-code
-      fira-code-symbols
-      liberation_ttf
-      mplus-outline-fonts.githubRelease
-      noto-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
-      proggyfonts
+#      corefonts
+#      dina-font
+#      fira-code
+#      fira-code-symbols
+#      liberation_ttf
+#      mplus-outline-fonts.githubRelease
+#      noto-fonts
+#      noto-fonts-cjk
+#      noto-fonts-emoji
+#      proggyfonts
       (nerdfonts.override {
         fonts = [
      "JetBrainsMono"
