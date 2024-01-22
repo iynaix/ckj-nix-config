5e136cbde900e1261dd8684131ef5f444e4244ab
CerealKillerJohn <JWRhine@proton.me>
2024-01-21 21:11:46 -0500

    edited neofetch config / removed some fetch pkgs

--- hosts/packages.nix
+++ hosts/packages.nix
@@ -13,21 +13,14 @@
     };
     shells = with pkgs; [ zsh ];
     systemPackages = with pkgs; [
-      bunnyfetch
       cbonsai
-      cpufetch
-      fastfetch
-      fetch-scm
       firefox
       kitty
-      leaf
       lm_sensors
       lsd
       mako
       neofetch
       neovim
-      nitch
-      owofetch
       wallust
       wget
     ];
