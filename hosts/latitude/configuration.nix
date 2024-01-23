e36700cb6783b31018bc7d9ec847a02c63008bd2
CerealKillerJohn <JWRhine@proton.me>
2024-01-22 23:51:41 -0500

    removed luks from file

--- hosts/latitude/configuration.nix
+++ hosts/latitude/configuration.nix
@@ -25,7 +25,7 @@
         enable = true;
       };
 #      luks.devices."luks-223bbc57-f4e3-44ab-983f-6d4a66789c94".device = "/dev/disk/by-uuid/223bbc57-f4e3-44ab-983f-6d4a66789c94";
-#    };
+    };
     kernelParams = ["quiet"];
     plymouth = {
       enable = true;
