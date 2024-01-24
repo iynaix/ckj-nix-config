8a52e814cca768aa6409c9e296bbb866af21c642
Lin Xianyi <iynaix@gmail.com>
2024-01-24 19:53:26 +0800

    Fix secrets

--- nixos/sops.nix
+++ nixos/sops.nix
@@ -7,7 +7,9 @@
   sops = {
     defaultSopsFile = ../hosts/secrets.yaml;
     defaultSopsFormat = "yaml";
+    gnupg.sshKeyPaths = [];
     age = {
+      sshKeyPaths = ["/home/jwrhine/.ssh/id_ed25519"];
       keyFile = "/home/jwrhine/.config/sops/age/keys.txt";
     };
     secrets = {
