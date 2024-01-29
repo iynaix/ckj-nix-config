9e869817059a6e68c5feb3543536d7b8e2e54fcd
CerealKillerJohn <JWRhine@proton.me>
2024-01-29 12:03:25 -0500

    added firefox.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+  ./firefox.nix
   ./shell/default.nix
   ];
 
@@ -74,14 +75,14 @@
     };
   };
 
-  home = {
-    file = {
-      ".librewolf/ca1rhbaa.default/chrome/userChrome.css" = {
-        source = ./chrome/userChrome.css;
-        # text = ''<the content of the file here''; if you want nix to create the file
-      };
-    };
-  };
+#  home = {
+#    file = {
+#      ".librewolf/ca1rhbaa.default/chrome/userChrome.css" = {
+#        source = ./chrome/userChrome.css;
+#        # text = ''<the content of the file here''; if you want nix to create the file
+#      };
+#    };
+#  };
 
   home.stateVersion = "23.11";
 }
