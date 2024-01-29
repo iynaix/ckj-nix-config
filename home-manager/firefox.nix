7ef91a0ac8e509e535d105c765bcb68e0c648d55
CerealKillerJohn <JWRhine@proton.me>
2024-01-29 13:00:26 -0500

    added settings to firefox.nix

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -4,28 +4,28 @@
   programs = {
     firefox= {
       enable = true;
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-      };
       profiles = {
         ${user} = {
           settings = {
+            "browser.startup.homepage" = "https//:mail.proton.me"
             "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
           };
-            userChrome = ''
-              @namespace url(http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul);
-              #navigator-toolbox {
-                height: 0px !important;
-                min-height: 0px !important;
-                overflow: hidden !important;
-              }
+          userChrome = ''
+            @namespace url(http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul);
+            #navigator-toolbox {
+              height: 0px !important;
+              min-height: 0px !important;
+              overflow: hidden !important;
+            }
 
-              #navigator-toolbox:focus,
-              #navigator-toolbox:focus-within,
-              #navigator-toolbox:active {
-                height: auto !important;
-                overflow: visible !important;
-              }
-            '';
+            #navigator-toolbox:focus,
+            #navigator-toolbox:focus-within,
+            #navigator-toolbox:active {
+              height: auto !important;
+              overflow: visible !important;
+            }
+          '';
+
         };
       };
     };
