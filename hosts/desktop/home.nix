5ac9c4a91dd831d91d714700caf8e0ea10b3ae1d
CerealKillerJohn <JWRhine@proton.me>
2024-01-30 13:17:54 -0500

    setting imports per host

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -2,7 +2,6 @@ _: {
   imports = [
     ./hardware-configuration.nix
     ./packages.nix
-    ../../home-manager/default.nix
     ../../modules/home-manager/default.nix
     ../../nixos/default.nix
     ../../nixos/xfce.nix
