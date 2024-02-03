c65695bd21bd33d6fdbd876048c6d3b543f4450e
CerealKillerJohn <JWRhine@proton.me>
2024-02-03 07:22:06 -0500

    removed laptop programs from amd

--- hosts/amd/packages.nix
+++ hosts/amd/packages.nix
@@ -3,10 +3,8 @@
 {
   environment = {
     systemPackages = with pkgs; [
-      acpi
       arc-theme
       breeze-plymouth
-      brightnessctl
 #      (callPackage ../../packages/bar-protonmail/bar-protonmail.nix {})
       (callPackage ../../packages/boxbuddyrs/boxbuddyrs.nix {})
       distrobox
@@ -20,7 +18,6 @@
       lm_sensors
       mpv
       obs-studio
-      parsec-bin
       pkgs.python311Full
       plymouth
       ranger
